f16219b02ef70ac9e4ff6760e3dfde0c24f3360c
Alonso2880 <a80alonso@gmail.com>
2025-06-23 13:05:25 +0200

    .

--- eMotivaMente/Assets/Scripts/EntrenaTuMenta/Nivel 1/DetectorColision.cs
+++ eMotivaMente/Assets/Scripts/EntrenaTuMenta/Nivel 1/DetectorColision.cs
@@ -4,12 +4,73 @@ using UnityEngine;
 
 public class DetectorColision : MonoBehaviour
 {
-    //public GameObject maleta;
+    public int puntospos=0;
+    public int puntosneg=0;
+
+    private GameObject esto;
+    private SpriteRenderer sp;
+
+    public bool parada = false; //Uso este bool para parar la corrutina
+    private void Start()
+    {
+        esto = this.gameObject;
+        sp = esto.GetComponent<SpriteRenderer>();
+        
+    }
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Maleta"))
         {
-            Debug.Log("a");
+            if(sp.sprite.name == "auriculares")
+            {
+                puntospos++;
+                parada = true;
+            }
+
+            if(sp.sprite.name == "camiseta")
+            {
+                puntospos++;
+            }
+
+            if(sp.sprite.name == "cushion-gbe1cbcd51_1280")
+            {
+                puntosneg++;
+            }
+
+            if(sp.sprite.name == "jabón")
+            {
+                puntospos++;
+            }
+
+            if(sp.sprite.name == "móvil")
+            {
+                puntospos++;
+            }
+
+            if(sp.sprite.name == "pantalón")
+            {
+                puntospos++;
+            }
+
+            if(sp.sprite.name == "paraguas")
+            {
+                puntosneg++;
+            }
+
+            if(sp.sprite.name == "peine")
+            {
+                puntospos++;
+            }
+
+            if(sp.sprite.name == "pelota")
+            {
+                puntosneg++;
+            }
+
+            if(sp.sprite.name == "zapatillas")
+            {
+                puntospos++;
+            }
         }
     }
 }
--- eMotivaMente/Assets/Scripts/EntrenaTuMenta/Nivel 1/N1_Actividad1.cs
+++ eMotivaMente/Assets/Scripts/EntrenaTuMenta/Nivel 1/N1_Actividad1.cs
@@ -4,10 +4,9 @@ using UnityEngine;
 
 public class N1_Actividad1 : MonoBehaviour
 {
-    [Header("Cursor personalizado")]
+   
     public GameObject cursorImage;
 
-    [Header("Generador de sprites")]
     public GameObject generador;
     public List<Sprite> imagenes;
 
@@ -15,9 +14,10 @@ public class N1_Actividad1 : MonoBehaviour
     private Collider2D generadorCollider;
     private Vector3 originalPosition;
 
-    [Header("Arrastre")]
     private bool isDragging = false;
     private Vector3 offset;
+
+    private BoxCollider2D boxCollider;
 
     void Awake()
     {
@@ -36,16 +36,19 @@ public class N1_Actividad1 : MonoBehaviour
     {
         // Inicia el ciclo de sprites
         StartCoroutine(Act1());
+        boxCollider = generador.GetComponent<BoxCollider2D>();
     }
 
     void Update()
     {
-        // Actualiza posición del cursor personalizado
+        DetectorColision GenS = generador.GetComponent<DetectorColision>();
+
+        //Actualiza posición del cursor personalizado
         Vector3 mouseWorld = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         mouseWorld.z = 0;
         cursorImage.transform.position = mouseWorld;
 
-        // Detecta clic inicial
+        //Detecta clic inicial
         if (Input.GetMouseButtonDown(0))
         {
             Collider2D hit = Physics2D.OverlapPoint(mouseWorld);
@@ -56,30 +59,67 @@ public class N1_Actividad1 : MonoBehaviour
             }
         }
 
-        // Arrastre
-        if (isDragging)
+        //Con este if lo que controlo es que si se detecta una colision entre la
+        //maleta y el sprite, el jugador ya no puede coger el objeto hasta que cambie
+        if (GenS.parada)
         {
-            generador.transform.position = mouseWorld + offset;
+
+        }
+        else
+        {
+           
+            if (isDragging) //Arrastrar el sprite
+            {
+                generador.transform.position = mouseWorld + offset;
+                boxCollider.enabled = false;
+            }
+
+            
+            if (Input.GetMouseButtonUp(0) && isDragging) //Suelto el sprite
+            {
+                boxCollider.enabled = true;
+                isDragging = false;
+            }
         }
 
-        // Suelta
-        if (Input.GetMouseButtonUp(0) && isDragging)
+        //Condición de victoria
+        if(GenS.puntospos == 7 && GenS.puntosneg == 0)
         {
-            isDragging = false;
-            // Si quieres que vuelva a la posición original al soltar, descomenta:
-            // generador.transform.position = originalPosition;
+            //Ha acertado al 100%
         }
+        else
+        {
+            if(GenS.puntospos <=7 && GenS.puntosneg <= 2)
+            {
+                //Esta bien pero con algunos fallos
+            }
+            else
+            {
+                if(GenS.puntosneg == 3)
+                {
+                    //esta mal
+                }
+            }
+        }
+ 
     }
 
+    //Corrutina que recorre la lista de sprites cada 7 segundos
     private IEnumerator Act1()
     {
+        DetectorColision GenS = generador.GetComponent<DetectorColision>();
         while (true)
         {
             foreach (var sprite in imagenes)
             {
+                GenS.parada = false;
                 generadorRenderer.sprite = sprite;
                 generador.transform.position = originalPosition;
+
+
                 yield return new WaitForSeconds(7f);
+                
+                
             }
         }
     }
